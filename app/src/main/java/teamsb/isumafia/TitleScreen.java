a2741020df033cee19621d5ef11cf4e443fa5fee
Adam <aheimen17@gmail.com>
2015-09-19 01:12:57 -0500

    initial commit

--- app/src/main/java/teamsb/isumafia/TitleScreen.java
+++ app/src/main/java/teamsb/isumafia/TitleScreen.java
@@ -1,17 +1,45 @@
 package teamsb.isumafia;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class TitleScreen extends AppCompatActivity {
+public class TitleScreen extends BaseGameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        if (isSignedIn())
+        {
+            textView.setText("You are signed in");
+        }
+        else {
+            textView.setText("You are not signed in");
+        }
+
+        final Button signoutButton = (Button) findViewById(R.id.button);
+        signoutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                signOut();
+            }
+        });
+
+        final Button signinButton = (Button) findViewById(R.id.button2);
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,5 +61,15 @@ public class TitleScreen extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSignInFailed() {
+
+    }
+
+    @Override
+    public void onSignInSucceeded() {
+        Toast.makeText(this, "Hello", Toast.LENGTH_LONG);
     }
 }
