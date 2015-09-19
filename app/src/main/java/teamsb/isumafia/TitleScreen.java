be49ce8acc55f6c21759e4ae9b68878ba354159c
Tyler Tran <ttran@iastate.edu>
2015-09-18 23:15:23 -0500

    Added RulesPage and HostLobby

--- app/src/main/java/teamsb/isumafia/TitleScreen.java
+++ app/src/main/java/teamsb/isumafia/TitleScreen.java
@@ -4,13 +4,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class TitleScreen extends AppCompatActivity {
+
+    Button btnHost, btnLogin, btnRules;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
+
+        btnHost = (Button) findViewById(R.id.buttonStart);
+        btnLogin = (Button) findViewById(R.id.buttonLogin);
+        btnRules = (Button) findViewById(R.id.buttonRules);
+
+        btnHost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), HostLobby.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRules.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), RulesPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(v.getContext(), HostLobby.class);
+                // startActivity(intent);
+            }
+        });
+
     }
 
     @Override
