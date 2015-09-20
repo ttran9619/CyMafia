65fb74a6aa5ee7e40c917111bdd4676b91b1e72b
Tyler Tran <ttran9619@gmail.com>
2015-09-20 01:28:58 -0500

    Testing Achievements on RulesPage

--- app/src/main/java/teamsb/isumafia/RulesPage.java
+++ app/src/main/java/teamsb/isumafia/RulesPage.java
@@ -1,14 +1,21 @@
 package teamsb.isumafia;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class RulesPage extends AppCompatActivity {
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.multiplayer.Multiplayer;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatchConfig;
+
+import java.util.ArrayList;
+
+public class RulesPage extends BaseGameActivity {
 
     Button btnBack;
 
@@ -16,6 +23,9 @@ public class RulesPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules_page);
+
+
+        Games.Achievements.unlock(getApiClient(), "CgkIxP26lfwBEAIQAQ");
 
         btnBack = (Button) findViewById(R.id.buttonRulesBack);
 
@@ -51,4 +61,58 @@ public class RulesPage extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    @Override
+    public void onActivityResult(int request, int response, Intent data)
+    {
+        super.onActivityResult(request, response, data);
+
+        if (request == 1)
+        {
+            if (response != Activity.RESULT_OK) {
+                // user canceled
+                return;
+            }
+
+            // Get the invitee list.
+            final ArrayList<String> invitees =
+                    data.getStringArrayListExtra(Games.EXTRA_PLAYER_IDS);
+
+            // Get auto-match criteria.
+            Bundle autoMatchCriteria = null;
+            int minAutoMatchPlayers = data.getIntExtra(
+                    Multiplayer.EXTRA_MIN_AUTOMATCH_PLAYERS, 0);
+            int maxAutoMatchPlayers = data.getIntExtra(
+                    Multiplayer.EXTRA_MAX_AUTOMATCH_PLAYERS, 0);
+            if (minAutoMatchPlayers > 0) {
+                autoMatchCriteria = RoomConfig.createAutoMatchCriteria(
+                        minAutoMatchPlayers, maxAutoMatchPlayers, 0);
+            } else {
+                autoMatchCriteria = null;
+            }
+
+            TurnBasedMatchConfig tbmc = TurnBasedMatchConfig.builder()
+                    .addInvitedPlayers(invitees)
+                    .build();
+
+            // Create and start the match.
+            Games.TurnBasedMultiplayer
+                    .createMatch(getApiClient(), tbmc)
+                    .setResultCallback(new MatchInitiatedCallback());
+        }
+    }
+
+    @Override
+    public void onSignInFailed() {
+
+    }
+
+    @Override
+    public void onSignInSucceeded() {
+
+    }
+
+
 }
