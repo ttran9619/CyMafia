f3cf272ef364a0cebd28d3c944ac7613f2e7d1b1
Tyler Tran <ttran9619@gmail.com>
2015-09-20 00:05:07 -0500

    Drafted the Game Over Screen

--- app/src/main/java/teamsb/isumafia/GameOverPage.java
+++ app/src/main/java/teamsb/isumafia/GameOverPage.java
@@ -1,23 +1,46 @@
 package teamsb.isumafia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameOverPage extends AppCompatActivity {
+
+    Button btnBack;
+    TextView textGameOverState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over_page);
 
+        Intent intentStarted = getIntent();
+        GameState GS = (GameState) intentStarted.getSerializableExtra("PassedGameState");
 
 
+        btnBack = (Button) findViewById(R.id.buttonGameOverBack);
 
 
+        textGameOverState = (TextView) findViewById(R.id.textGameOverState);
 
+        if(GS.mafiaWin==true){
+            textGameOverState.setText(getString(R.string.mafia_win));
+        }else if(GS.citizenWin==true){
+            textGameOverState.setText(getString(R.string.citizen_win));
+        }
 
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), TitleScreen.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
